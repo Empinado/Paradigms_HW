6d68c739595fb0a3443558c32908966982ab2705
Empinado <116734339+Empinado@users.noreply.github.com>
2023-12-11 22:06:24 +0500

    added HW_5

---
+++ Paradigms_HW_5/src/HW_6/HW_5.pl
@@ -0,0 +1,6 @@
+
+sum([], 0).
+sum([FirstElement|OtherElements], Sum) :-
+    sum(OtherElements, CurrentSum), Sum is CurrentSum + FirstElement.
+
+?- sum([6,4,7,3,9,1], SumAllElements), write(SumAllElements).
